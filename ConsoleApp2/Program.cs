3573e0f1a6cf25a875cd7cf59dca7b470d239290
ra2357028 <ra2357028@LDI304>
2024-02-26 09:09:51 -0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿double[]salarios = new double[10];
+string? j;
+double maiorSal = 0;
+for (int i = 0; i < salarios.Length; i++)
+{
+    Console.Write((i+1)+"º salário:");
+    j = Console.ReadLine();
+    Double.TryParse(j, out salarios[i]);
+    if (salarios[i] > maiorSal)
+    {
+        maiorSal = salarios[i];
+    }
+}
+Console.Write("Maior salário: R$" + maiorSal.ToString("0.00"));
